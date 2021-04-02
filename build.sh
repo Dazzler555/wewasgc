5fe5f458a094e38ec5434f30ee87b5d9697aca9b
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2021-04-02 13:13:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ repo sync -j$(nproc --all)
 git clone https://github.com/SHRP-Devices/device_xiaomi_violet.git -b android-9.0 device/xiaomi/violet
 
 rm -rf out
-source build/envsetup.sh && lunch omni_violet-eng && export LC_ALL="C" && export ALLOW_MISSING_DEPENDENCIES=true && mka recoveryimage
+. build/envsetup.sh && lunch omni_violet-eng && export LC_ALL="C" && export ALLOW_MISSING_DEPENDENCIES=true && mka recoveryimage
 
 cd out/target/product/violet
 curl -sL https://git.io/file-transfer | sh
