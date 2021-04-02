f9f634c4db1b0438d31da120b301924e1af90038
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2021-04-02 14:14:06 +0530

    9.0

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 mkdir -p /tmp/recovery
 cd /tmp/recovery
 
-repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 -g default,-device,-mips,-darwin,-notdefault 
+repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0 -g default,-device,-mips,-darwin,-notdefault 
 repo sync -j$(nproc --all)
 
 
-git clone https://github.com/Brock5555/twrp_device_xiaomi_violet.git -b android-10 device/xiaomi/violet
+git clone https://github.com/Brock5555/twrp_device_xiaomi_violet.git -b android-9.0 device/xiaomi/violet
 
 rm -rf out
 . build/envsetup.sh && lunch omni_violet-eng && export ALLOW_MISSING_DEPENDENCIES=true && mka recoveryimage
