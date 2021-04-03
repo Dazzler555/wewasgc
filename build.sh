d8519f989956ee1fab9ac33a1073cfd729202fbe
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2021-04-04 02:42:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -41,5 +41,5 @@ tg(){
 	curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
 }
 
-send_zip=$(up out/target/product/violet/*zip) && tg $id "Build Succeed!
+send_zip=$(up out/target/product/violet/*img) && tg $id "Build Succeed!
 $send_zip
