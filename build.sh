ea2a6219ae04cd1cda87954264b7174cba2f03e0
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-04-04 01:10:58 +0530

    retry

--- build.sh
+++ build.sh
@@ -29,5 +29,7 @@ curl -sL https://git.io/file-transfer | sh
 ./transfer wet *.zip
 ./transfer wet recovery.img
 
-ZIPNAME="SHRP_v3.0_stable-Official_violet_*.zip"
-curl -F chat_id=1033360588 -F document=@$ZIPNAME -F parse_mode=markdown https://api.telegram.org/bot1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U/sendDocument
+
+#curl -F chat_id=1033360588 -F document=*.zip -F parse_mode=markdown https://api.telegram.org/bot1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U/sendDocument
+
+curl -F document=@"*.zip" https://api.telegram.org/bot1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U/sendDocument?chat_id=1033360588
