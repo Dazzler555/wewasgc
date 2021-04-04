e34c6c180f2b79135934f75648ba13ecb3841685
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-04-04 14:05:33 +0530

    push

--- build.sh
+++ build.sh
@@ -15,13 +15,13 @@ tg(){
 
 id=1033360588
 
-tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "Your Tmate XD" && tg $id "$send_shell"
+tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "Drone starded :)" && tg $id "$send_shell"
 
-repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0 -g default,-device,-mips,-darwin,-notdefault 
+repo init --depth=1 -u  git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_9.0 -g default,-device,-mips,-darwin,-notdefault 
 repo sync -j$(nproc --all)
 
 
-git clone https://github.com/Brock5555/twrp_device_xiaomi_violet.git -b android-9.0 device/xiaomi/violet
+git clone https://github.com/SHRP-Devices/device_xiaomi_violet.git -b android-9.0 device/xiaomi/violet
 
 rm -rf out
 . build/envsetup.sh && lunch omni_violet-eng && export ALLOW_MISSING_DEPENDENCIES=true && mka recoveryimage
@@ -32,20 +32,12 @@ curl -sL https://git.io/file-transfer | sh
 ./transfer wet *.zip
 ./transfer wet recovery.img
 
-zip=$(./transfer wet *.zip > zip.txt)
-img=$(./transfer wet recovery.img > img.txt)
+apt install wget -y
 
-up(){
-	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
-	# 14 days, 10 GB limit
-}
-tg(){
-	bot_api=1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U
-	your_telegram_id=$1 # No need to touch 
-	msg=$2 # No need to touch
-	curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
-}
-PWD(){
-      ~/out/target/product/violet/
-     }
-send_zip=$(up PWD/*img) && tg $id "Build Succeed! $zip $img $send_zip"
+wget https://raw.githubusercontent.com/tazz555/ntausjdohtajatis/main/rsfhsuwf.conf
+
+apt install python3-pip -y
+
+pip3 install telegram-send
+
+telegram-send --config rsfhsuwf.conf --file *.zip --caption "Build completed Successfully :)"
