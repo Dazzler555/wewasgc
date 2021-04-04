4d8326d3407052e704bdd1570e22ecf63f505585
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-04-04 14:20:20 +0530

    clean

--- tg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -l -c
-
-cd out/target/product/violet
-
-ZIPNAME="SHRP_v3.0_stable-Official_violet_*.zip"
-curl -F chat_id=1033360588 -F document=@$ZIPNAME -F parse_mode=markdown https://api.telegram.org/bot1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U/sendDocument
