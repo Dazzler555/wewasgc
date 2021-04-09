79010895f5ba8f17fe84864566e28e0f7c68fb9a
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2021-04-09 14:46:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-r
 repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_9.0 -g default,-device,-mips,-darwin,-notdefault 
 repo sync -j$(nproc --all)
 
-git clone https://github.com/SHRP-Devices/device_xiaomi_violet.git device/xiaomi/violet
+git clone https://github.com/Dazzler555/no-cp.git device/xiaomi/violet
 
 rm -rf out
 . build/envsetup.sh && lunch omni_violet-eng && export LC_ALL="C" && export ALLOW_MISSING_DEPENDENCIES=true && mka recoveryimage
