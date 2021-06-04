5fc20de9737fe5f38e9a915c1bc2081bd4f9be26
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2021-06-04 13:31:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,21 +7,20 @@ mkdir -p /tmp/recovery
 cd /tmp/recovery
 
 sudo apt install git -y
+# curl https://storage.googleapis.com/git-repo-downloads/repo > repo
+
+
+sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+sudo chmod a+rx /usr/local/bin/repo
 
 repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_9.0 -g default,-device,-mips,-darwin,-notdefault
 
-# repo init --depth=1 -u git://github.com/PitchBlackRecoveryProject/manifest_pb -b android-10.0 -g default,-device,-mips,-darwin,-notdefault 
-
-# repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0
-
 repo sync -j$(nproc --all)
 
-
-# git clone https://github.com/Brock5555/shrp_device_xiaomi_surya.git -b twrp device/xiaomi/surya
-# git clone https://github.com/Joker-V2/shrp_device_xiaomi_surya.git -b android-10 device/xiaomi/surya
 git clone https://github.com/SHRP-Devices/device_xiaomi_violet.git -b android-9.0 device/xiaomi/violet
-rm -rf out
 
+rm -rf out
+cd device/xiaomi/violet
 . build/envsetup.sh && lunch omni_violet-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
